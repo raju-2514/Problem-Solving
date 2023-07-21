89c3a4c071f646447dab37afd684c366b4106ae3
Md.Raju Biswas <91734699+raju-2514@users.noreply.github.com>
2023-07-21 21:05:33 +0600

    Add files via upload

---
+++ Java_Int_to_String.java
@@ -0,0 +1,70 @@
+package HackerRank;
+
+import java.util.Scanner;
+
+public class Java_Int_to_String {
+    public static void main(String[] args) {
+        Scanner scan=new Scanner(System.in);
+        int a;
+        a=scan.nextInt();
+      String s=String.valueOf(a);
+      if(a==Integer.parseInt(s))
+      {
+          System.out.println("Good job");
+      }
+      else {
+          System.out.println("Wrong answer");
+      }
+        
+    }
+
+
+//    import java.util.*;
+//import java.security.*;
+//    public class Solution {
+//        public static void main(String[] args) {
+//
+//            DoNotTerminate.forbidExit();
+//
+//            try {
+//                Scanner in = new Scanner(System.in);
+//                int n = in .nextInt();
+//                in.close();
+//                //String s=???; Complete this line below
+//
+//                String s=String.valueOf(n);
+//
+//
+//                if (n == Integer.parseInt(s)) {
+//                    System.out.println("Good job");
+//                } else {
+//                    System.out.println("Wrong answer.");
+//                }
+//            } catch (DoNotTerminate.ExitTrappedException e) {
+//                System.out.println("Unsuccessful Termination!!");
+//            }
+//        }
+//    }
+
+    //The following class will prevent you from terminating the code using exit(0)!
+//    class DoNotTerminate {
+//
+//        public static class ExitTrappedException extends SecurityException {
+//
+//            private static final long serialVersionUID = 1;
+//        }
+//
+//        public static void forbidExit() {
+//            final SecurityManager securityManager = new SecurityManager() {
+//                @Override
+//                public void checkPermission(Permission permission) {
+//                    if (permission.getName().contains("exitVM")) {
+//                        throw new ExitTrappedException();
+//                    }
+//                }
+//            };
+//            System.setSecurityManager(securityManager);
+//        }
+//    }
+
+}
