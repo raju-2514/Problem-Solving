bf64626290ae9af52300d1f0119f418da68412d7
Md.Raju Biswas <91734699+raju-2514@users.noreply.github.com>
2023-07-23 14:44:35 +0600

    Add files via upload

--- Date_and_Time.java
+++ Date_and_Time.java
@@ -9,15 +9,16 @@ public class Date_and_Time {
         Calendar calendar = Calendar.getInstance();
 
         // Set the year, month (0-based), and day
-        calendar.set(year, month - 1, day);
+        calendar.set(year, month-1, day);
 
         // Get the day of the week (1 = Sunday, 2 = Monday, ..., 7 = Saturday)
         int dayOfWeek = calendar.get(Calendar.DAY_OF_WEEK);
 
         // Convert the numeric representation of the day to the corresponding string representation
         String[] daysOfWeek = {"SUNDAY", "MONDAY", "TUESDAY", "WEDNESDAY", "THURSDAY", "FRIDAY", "SATURDAY"};
-        return daysOfWeek[dayOfWeek];
+        return daysOfWeek[dayOfWeek-1];
     }
+
 
     public static void main(String[] args) {
         Scanner scanner=new Scanner(System.in);
